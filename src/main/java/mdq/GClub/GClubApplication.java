ef0bf8e163157d01d42916ace250891bd97a10a2
Eze Morales <ezequielmorales5@icloud.com>
2021-09-04 17:45:58 -0300

    Spring init

---
+++ src/main/java/mdq/GClub/GClubApplication.java
@@ -0,0 +1,13 @@
+package mdq.GClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mdq/GClub/GClubApplicationTests.java
@@ -0,0 +1,13 @@
+package mdq.GClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
